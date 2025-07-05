720038db8fa455f558937af698d8c8634fc60fe5
Ganesh Podishetti <ganeshpodishetti@gmail.com>
2025-07-04 22:21:50 -0400

    Refactor Vite configuration to load API URL from environment variables

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,33 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import process from "process";
 import { fileURLToPath } from 'url';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from "vite";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://studentperks-dev.azurewebsites.net",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  const apiUrl =
+    env.VITE_API_URL;
+
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      proxy: {
+        "/api": {
+          target: apiUrl,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
       },
     },
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+  };
 });
